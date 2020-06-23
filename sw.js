936a8179fb57365582e87baf4f7c7773861c14e3
artpi <artur.piszek@gmail.com>
2020-06-23 10:51:06 +0200

    ServiceWorker to the resque

--- sw.js
+++ sw.js
@@ -1,44 +1,39 @@
-self.addEventListener('install', function(e) {
-    e.waitUntil(
-      caches.open('podcast-app').then(function(cache) {
-        return cache.addAll([
-          './player.htm',
-          './podcasts.webmanifest',
-          './apple-touch-icon.png',
-          './sw.js',
-          './'
-        ]);
-      }).catch( err => console.log(err))
-    );
-   });
-   
+const currentVersion = 'app-v2';
 
-   self.addEventListener('fetch', function(event) {
-     if ( event.request.url.indexOf('.mp3') !== -1 ) {
-        //mp3 file. Cache first, do not redownload.
-        return event.respondWith(
-          caches.open( 'media' ).then(function(cache) {
-            return cache.match(event.request).then(function (response) {
-              return response || fetch(event.request).then(function(response) {
-                cache.put(event.request, response.clone());
-                return response;
-              });
-            });
-          })
-        );      
-     }
-      event.respondWith(
-        caches.open('podcast-app').then(function(cache) {
-          return fetch(event.request).then(function(response) {
-            cache.put(event.request, response.clone());
-            return response;
-          }).catch(function() {
-            return caches.match(event.request);
-          });
-        })
-      );
-  });
-  
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(currentVersion).then(function (cache) {
+      return cache.addAll([
+        './player.htm',
+        './podcasts.webmanifest',
+        './assets/localforage.nopromises.min.js',
+        './apple-touch-icon.png',
+        './favicon.ico',
+      ]);
+    }).catch(err => console.log(err))
+  );
+});
 
-  
-   
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  // delete any caches that aren't in expectedCaches
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== currentVersion) {
+          return caches.delete(key);
+        }
+      })
+    )).then(() => {
+      console.log(currentVersion + ' now ready to handle fetches!');
+    })
+  );
+});
